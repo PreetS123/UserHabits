0f20ede2af6ccdbab212e57cf9c9c0e81949307d
PreetS123 <preetbhardwaj517@gmail.com>
2023-01-08 13:13:51 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,14 @@ import { Habits } from './Pages/Habits';
 import { Profile } from './Pages/Profile';
 import { Signup } from './Pages/Signup';
 import { Login } from './Pages/Login';
+import { Navbar } from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
+       <Navbar/>
               <Routes>
+               
                 <Route path='/profile' element={<Profile/>}   />
                 <Route path='/login' element={<Login/>}   />
                 <Route path='/' element={<Signup/>}   />
